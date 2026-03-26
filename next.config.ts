c69ad4f6534db92246d223868dcf3fd5b10fb9f7
K.samuel <fiatechnologiecam@gmail.com>
2026-03-26 16:52:09 +0000

    correction de l erreur sur versel

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  output: isProd ? "export" : undefined, 
+  output: (isProd && !process.env.VERCEL) ? "export" : undefined, 
   distDir: "out",
   images: {
     unoptimized: true,
